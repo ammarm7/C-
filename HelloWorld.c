c1eac5358b781957a5bf1095c462471d22cccb8b
ammarm7 <ammarm7@gmail.com>
2017-01-13 19:04:31 -0500

    Added a HelloWorld C++ program

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;	//You need to scope in the standard namespace, otherwise it will not know what things like "cout" and "endl" are
+
+int main()
+{
+	
+	cout << "Hello world" << endl;	//This is called the "stream insertion" operator. 
+	
+
+
+	return 0;
+}
