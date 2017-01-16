439ad4e6f9c7a2bd41f74aa6228b54d2b046af3c
ammarm7 <ammarm7@gmail.com>
2017-01-15 20:07:38 -0500

    Added a simple example of passing by ref in C++

---
+++ PassByRef.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+bool doublePerc(int);
+bool doublePerc(int*);
+
+
+int main()
+{
+	int num;
+	bool inputOk = false;
+
+	while (!inputOk) {
+		cout << "Please enter a number betweeen 0 and 100; ";
+		cin >> num;
+		inputOk = doublePerc(&num);
+	}
+
+	cout << "The result is " << num << endl;
+
+	return 0;
+} 
+
+bool doublePerc(int n)
+{
+	if (n < 0 || n > 100) 
+		return false;
+
+	n = 2;
+	return true;
+}
+
+bool doublePerc(int* n)
+{
+	if (*n < 0 || *n > 100) 
+		return false;
+
+	*n *= 2;
+	return true; 
+
+}
+
+
+
+
+
